b5af5d5c3349becd1eff10de2c0d1c7ecb7ecb05
JunH0128 <C24519656@myTUDublin.ie>
2024-12-04 23:32:32 +0000

    first commit added the sprite

---
+++ arrow.gd
@@ -0,0 +1,9 @@
+extends Sprite2D
+
+@export var key_name: String = ""
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed(key_name):
+		print(key_name)
