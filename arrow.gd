edf4ede88b41870c14c718d91494f2c732eeedb0
JunH0128 <C24519656@myTUDublin.ie>
2024-12-05 18:30:38 +0000

    added the points working

--- arrow.gd
+++ arrow.gd
@@ -1,9 +1,15 @@
 extends Sprite2D
 
+@onready var falling_key = preload("res://point.tscn")
 @export var key_name: String = ""
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed(key_name):
-		print(key_name)
+		CreateFallingKey()
+
+func CreateFallingKey():
+	var point = falling_key.instantiate()
+	get_tree().get_root().call_deferred("add_child",point)
+	point.Setup(position.x, frame + 4)
